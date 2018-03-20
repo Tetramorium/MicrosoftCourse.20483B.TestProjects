9acab0e0e978d2dd61176ed8bc690edd481bb38e
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2018-03-20 14:43:39 +0100

    First Commit

---
+++ Animal/Entity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Animal
+{
+    public abstract class Entity
+    {
+        public string Name { get; set; }
+        private IGreetingBehaviour GreetingBehaviour;
+
+        public Entity(string _Name, IGreetingBehaviour _GreetingBehaviour)
+        {
+            this.Name = _Name;
+            this.GreetingBehaviour = _GreetingBehaviour;
+        }
+
+        public void IntroduceHumanSubject()
+        {
+            Console.WriteLine(string.Format("{0} : {1}", this.Name, this.GreetingBehaviour.Greet()));
+        }
+    }
+
+    public interface IGreetingBehaviour
+    {
+        string Greet();
+    }
+
+    public class GreetIgnore : IGreetingBehaviour
+    {
+        public string Greet()
+        {
+            return "Ignoring you!";
+        }
+    }
+
+    public class GreetWelcome : IGreetingBehaviour
+    {
+        public string Greet()
+        {
+            return "Jumping and barking!";
+        }
+    }
+
+    public class Dog : Entity
+    {
+        public Dog(string _Name, IGreetingBehaviour _GreetingBehaviour) : base(_Name, _GreetingBehaviour)
+        {
+        }
+    }
+
+    public class Cat : Entity
+    {
+        public Cat(string _Name, IGreetingBehaviour _GreetingBehaviour) : base(_Name, _GreetingBehaviour)
+        {
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Animal;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 2;
+            string str = "abc";
+
+            Print(b, nameof(b));
+            Print(str, nameof(str));
+
+            b = b << 1;
+
+            Print(b, nameof(b));
+
+            Dog d = new Dog("Bob", new GreetWelcome());
+            Print(d, nameof(d));
+            d.IntroduceHumanSubject();            
+
+            Cat c = new Cat("Milou", new GreetIgnore());
+            c.IntroduceHumanSubject();
+            Print(c, nameof(c));
+
+            Entity e = c as Entity;
+            e.IntroduceHumanSubject();
+            Print(e, nameof(e));
+
+            feed(e);
+
+            feed(d);
+        }
+
+        static void Print<T>(T _Obj, string _Identifier)
+        {
+            Console.WriteLine(_Obj.GetType() + " : " + _Identifier + " : " + _Obj.ToString());
+
+            PrintLine();
+        }
+
+        static void PrintLine()
+        {
+            Console.WriteLine("\n----------------------\n");
+        }
+
+        static void feed(Entity t)
+        {
+            switch (t.GetType())
+            {
+                case Type DogType when DogType == typeof(Dog):
+                    Console.WriteLine("Give a bone");
+                    PrintLine();
+                    break;
+                case Type CatType when CatType == typeof(Cat):
+                    Console.WriteLine("Give a fish");
+                    PrintLine();
+                    break;
+            }
+        }
+    }
+}
