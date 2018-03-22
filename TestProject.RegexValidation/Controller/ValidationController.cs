b05c5277dce2c817bbd55f2f91f8c6214d9234b2
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2018-03-22 14:30:30 +0100

    More testing

---
+++ TestProject.RegexValidation/Controller/ValidationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestProject.RegexValidation.Controller
+{
+    public class ValidationController
+    {
+        public static bool OnlyNumbers(string _Input)
+        {
+            return Regex.IsMatch(_Input.Replace(" ", ""), @"^[0-9]+$");
+        }
+
+        public static bool IsValidPassword(string _Input)
+        {
+            // ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$ No special character
+            return Regex.IsMatch(_Input, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$");
+        }
+
+        public static int ParseInt(string _Input)
+        {
+            try
+            {
+                return int.Parse(Regex.Match(_Input, @"\d+").Value);
+            }
+            catch
+            {
+                return -1;
+            }
+
+        }
+    }
+}
---
+++ TestProject.RegexValidation/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestProject.RegexValidation.Controller;
+
+namespace TestProject.RegexValidation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Bt_TestOnlyNumbers(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(this.tb_OnlyNumbers.Text))
+            {
+                if (ValidationController.OnlyNumbers(this.tb_OnlyNumbers.Text.Trim()))
+                    MessageBox.Show("These are only numbers!");
+                else
+                    MessageBox.Show("These are not only numbers!");
+            }
+            else
+            {
+                MessageBox.Show("These are not numbers...");
+            }
+        }
+
+        private void OnlyNumbers_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !ValidationController.OnlyNumbers(e.Text);
+        }
+
+        private void bt_TestPassword_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidationController.IsValidPassword(this.pb_Password.Password))
+                MessageBox.Show("Valid password!");
+            else
+                MessageBox.Show("Password must be between 8 and 15 characters and requires one special character, one uppercase, one lowercase and one number!");
+        }
+
+        private void bt_TestParseInt_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(ValidationController.ParseInt(this.tb_TestParseInt.Text).ToString());       
+        }
+    }
+}
