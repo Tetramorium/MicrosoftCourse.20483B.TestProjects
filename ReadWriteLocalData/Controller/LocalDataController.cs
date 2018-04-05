10cbd7ee67a7bffdd329e6855c45ea98eaaba3e7
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2018-04-05 15:03:50 +0200

    Added ReadWriteLocalData

---
+++ ReadWriteLocalData/Controller/LocalDataController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWriteLocalData.Controller
+{
+    public class LocalDataController
+    {
+        private string tempDirectoryPath = Path.GetTempPath() + @"\ReadWriteLocalData";
+        private string tempLogPath = @"\Logs";
+        private string tempFileName = @"\log.txt";
+
+        public ObservableCollection<string> Logs { get; set; }
+
+        public LocalDataController()
+        {
+            Directory.CreateDirectory(tempDirectoryPath + tempLogPath);                   
+        }
+
+        public void Write(string str)
+        {
+            File.AppendAllText(tempDirectoryPath + tempLogPath + tempFileName, str + Environment.NewLine);
+        }
+
+        public void Read()
+        {
+            if (File.Exists(tempDirectoryPath + tempLogPath + tempFileName))
+                this.Logs = new ObservableCollection<string>(File.ReadAllLines(tempDirectoryPath + tempLogPath + tempFileName));
+        }
+    }
+}
---
+++ ReadWriteLocalData/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using ReadWriteLocalData.Controller;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReadWriteLocalData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LocalDataController ldc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = ldc;
+
+            this.ldc = new LocalDataController();
+
+            ldc.Read();
+
+            this.lb_Logs.ItemsSource = ldc.Logs;
+        }
+
+        private void bt_LogComment_Click(object sender, RoutedEventArgs e)
+        {
+            ldc.Write(DateTime.Now.ToString() + " - Test");
+
+            ldc.Read();
+
+            this.lb_Logs.ItemsSource = ldc.Logs;
+        }
+    }
+}
